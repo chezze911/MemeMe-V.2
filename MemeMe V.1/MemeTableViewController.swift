c07b56740d577b16d70127842a7cc9c7dd682c03
chezze911 <chinguyen@chis-mbp.attlocal.net>
2017-03-02 18:17:06 -0800

    Fixed issue with startup when meme array is empty

---
+++ MemeMe V.1/MemeTableViewController.swift
@@ -0,0 +1,61 @@
+//
+//  MemeTableViewController.swift
+//  MemeMe V.1
+//
+//  Created by Chi Nguyen on 2017/02/22.
+//  Copyright © 2017 udacity. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+class MemeTableViewController: UITableViewController{
+    
+    var memes:[Meme]!
+
+    let addMemeIdentifer = "MemeEditorViewController"
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        let appDelegate = UIApplication.shared.delegate as! AppDelegate
+        memes = appDelegate.memes
+        
+    }
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewDidLoad()
+        //Go straight to editor view if no memes
+        if memes.isEmpty {
+            performSegue(withIdentifier: addMemeIdentifer, sender: nil)
+        }
+    }
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?){
+        if (segue.identifier == addMemeIdentifer) {
+            _ = segue.destination as! MemeEditorViewController
+        }
+    }
+    
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return memes.count
+    }
+    
+    //Configure tableView cells
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "MemeTableViewCell") as UITableViewCell!
+        let meme = memes[indexPath.row]
+        
+        cell?.imageView?.image = meme.memedImage
+        cell?.textLabel?.text = "\(meme.topTextField) \(meme.bottomTextField)"
+        
+        return cell!
+    }
+    //Show detailView from selection
+    func tableView(tableView: UITableView, didSelectRowAtIndexPath indexPath: NSIndexPath) {
+        let object = storyboard?.instantiateViewController(withIdentifier: "MemeDetailViewController")
+        let detailVC = object as! MemeDetailViewController
+        
+        //pass data from selected row to detail View
+        detailVC.meme = memes[indexPath.row]
+        self.navigationController!.pushViewController(detailVC, animated: true)
+    }
+  
+}
